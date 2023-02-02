74434596633e616997180f385f0a4c3e82adfe76
nastyas33 <nastassia.sasnouskaya@gmail.com>
2023-02-02 19:50:07 +0300

    Update project skeleton

--- blog/news_feed/views.py
+++ blog/news_feed/views.py
@@ -5,26 +5,16 @@ from .models import Page, Post
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, DeleteView, CreateView, UpdateView
 
-
-def say_hello(request):
-    return HttpResponse("Hello")
-
-
-def get_pages_list(request):
-    context = {"all_pages": Page.objects.all()}
-    return render(request, "all_pages.html", context)
+#class PostListView(ListView):
+    #model = Post
+    #template_name = "all_posts.html"
 
 
-class PostListView(ListView):
-    model = Post
-    template_name = "all_posts.html"
-
-
-class PostCreateView(CreateView):
-    model = Post
-    fields = ["name", "content", "page"]
-    template_name = "post_create.html"
-    success_url = "/"
+#class PostCreateView(CreateView):
+    #model = Post
+    #fields = ["name", "content", "page"]
+    #template_name = "post_create.html"
+    #success_url = "/"
 
 
 class PostDetailView(DetailView):
